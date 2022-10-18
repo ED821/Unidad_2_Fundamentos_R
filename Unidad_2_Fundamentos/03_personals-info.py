63080804734cc224f0546a1949209e86c8df9349
ED821 <106194445+ED821@users.noreply.github.com>
2022-10-18 09:21:00 -0500

    Add files via upload

---
+++ Unidad_2_Fundamentos/03_personals-info.py
@@ -0,0 +1,15 @@
+''' 
+Autor:Edgar Francisco Hernandez Mendez
+Fecha:18/10/22
+Actividad: Cree un script que solicite 4 datos, como: nombre,apellido,ubicacion y edad
+
+'''
+
+firstName = input ("¿Cual es tu nombre ? ")
+lastName = input ("¿Cual es tu apellido ?")
+location = input ("¿Cual es tu ubicacion ?")
+age = input ("¿Cuantos años ties ?")
+sapace = " "
+
+
+print("Hola!", firstName, lastName, "from", location, "from", age, "years")
