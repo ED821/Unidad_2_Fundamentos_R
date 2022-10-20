d4b0581379df2c083ca1ed16390e581868e7bb28
ED821 <106194445+ED821@users.noreply.github.com>
2022-10-20 09:15:20 -0500

    Add files via upload

--- Unidad_2_Fundamentos/07_find_devices.py
+++ Unidad_2_Fundamentos/07_find_devices.py
@@ -8,33 +8,14 @@ ACtividad:Create an empty list.
 
 
 
-#file=open("devices.txt","r")
-#for line in file:
- #   line = line.strip()
-  #  if "Switch" in line:
-   #      print(line)
-#file.close()
-
-
-
-
-file = open("devices.txt", "a")
-while True:
- newItem = input("Nombre del dispositivo: ")
- if newItem == "exit":
-    print("All done!")
- break
- file.write(newItem + "\n")
+file=open("devices.txt","r")
+for line in file:
+    line = line.strip()
+    if "Switch" in line:
+         print(line)
 file.close()
 
-#devices=[]
-#file=open(-"devices.txt","r")
-#for item in file:
- #   if "Cual es dispositivo a encontrar" in item.strip:
-  #  print(item.strip)
-  #devices.append(item.strip())
-#file.close()
-#print(devices)
+
 
 
 
